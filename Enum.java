63aefbc1fd8dec7303f080d075a2f8e3ab4c798f
Pratheek4 <pratheek2821@gmail.com>
2023-11-27 22:40:57 +0530

    gthb

---
+++ Enum.java
@@ -0,0 +1,34 @@
+enum Laptop{
+
+    Mackbook(3000), Xps(2200), Surface(1255), ThinhPad(); 
+    // These are called objects, and then they are calling parameterized constructor
+
+    private int price;
+
+    private Laptop(int price) {  // For the price given in the above objects
+        this.price = price;
+    }
+
+    private Laptop() {   // For object which do not have price
+        price  = 500;
+    }
+
+    public int getPrice() {  // To get the price
+        return price;
+    }
+
+    public void setPrice(int price) { // To set the price
+        this.price = price;
+    }
+
+    
+}
+
+public class Enum {
+    
+    public static void main(String[] args) {
+        
+        Laptop l = Laptop.Mackbook;
+        System.out.println(l);
+    }
+}
